4c47b298f3b2f0bded4b83a42fff7358dca345c8
Snehal-VibhuteN <snehalvibhut06@gmail.com>
2023-07-28 14:43:33 +0530

    Second Commit

--- src/test/java/day22June23/Test1.java
+++ src/test/java/day22June23/Test1.java
@@ -5,6 +5,9 @@ public class Test1 {
 	public static void main(String[] args) {
 		System.out.println("Hi.... First class First line");
 		int a=10;
+		float b=12.23f;
+		for(b=0.1f; b<=0.9; b+=0.1)
+			System.out.println(b);
 	}
 
 }
